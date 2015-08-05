3195957bc93d9093c1804067eab509cc21abf16a
codeamac01 <codeamac01@gmail.com>
2015-08-04 19:58:12 -0500

    99 elefantes

---
+++ elefante.rb
@@ -0,0 +1,14 @@
+def elefantes
+	min = 1
+	max = 100
+	while min < max do
+		if min == 1
+			puts "#{min} elefante se columpiaba sobre la tela de una araña, como veía que resistía fuero a llamar a otro elefante."
+		else
+			puts "#{min} elefantes se columpiaban"
+		end
+		min += 1	
+	end
+end
+
+elefantes 
